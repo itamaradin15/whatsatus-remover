1d260acfec9d7e07a27ff5e426ca2fe98ea2bed2
itamaradin15 <itamaradin15@gmail.com>
2022-09-25 11:16:21 -0500

    Fix error in new Whatsapp web version

--- content.js
+++ content.js
@@ -1,8 +1,9 @@
 
 
 var checkExist = setInterval(function() {
-    if (document.getElementsByClassName('_2cNrC').length) {
-       document.querySelector('._2cNrC').style.display = 'none';
+    if (document.querySelector('[data-testid="menu-bar-status"]') !== null) {
+        document.querySelector('[data-testid="menu-bar-status"]').style.display = 'none';
        clearInterval(checkExist);
     }
  }, 100)
+
